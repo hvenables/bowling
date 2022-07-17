6be9070df85135a25d71d87e5b2261008b67a50c
Harry Venables <harry.venables@immersivelabs.com>
2022-07-17 18:03:37 +0100

    dont love this solution

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,41 @@
 class Game
+  def initialize
+    @roles = []
+    @current_role = 0
+  end
+
+  def roll(x)
+    @roles[@current_role] = x
+    @current_role += 1
+  end
+
+  def score
+    index = 0
+    score = 0
+
+    10.times do
+      if strike?(index)
+        score += 10 + @roles[index + 1] + @roles[index + 2]
+        index += 1
+      elsif spare?(index)
+        score += 10 + @roles[index + 2]
+        index += 2
+      else
+        score += @roles[index] + @roles[index + 1]
+        index += 2
+      end
+    end
+
+    score
+  end
+
+  private
+
+  def spare?(index)
+    @roles[index] + @roles[index + 1] == 10
+  end
+
+  def strike?(index)
+    @roles[index] == 10
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe Game do
   let(:game) { described_class.new }
 
+  def roll_many(game, n, pins)
+    n.times { game.roll(pins) }
+  end
+
   it "will score 0 for an all gutter game" do
     20.times { game.roll(0) }
     expect(game.score).to eq 0
@@ -9,6 +13,14 @@ RSpec.describe Game do
   it "will score 20 for an all ones game" do
     20.times { game.roll(1) }
     expect(game.score).to eq 20
+  end
+
+  it "will correctly work out a spare" do
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    roll_many(game, 17, 0)
+    expect(game.score).to eq 16
   end
 
   it "will score 150 for an all fives game" do
